beb5a0e4fbbf6ff273544b506a2bd733828bc61e
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-11-03 20:41:21 -0500

    added sql driver dependency

--- twittertrends.go
+++ twittertrends.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/chimeracoder/anaconda"
+	"time"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func check(e error) {
@@ -11,12 +14,21 @@ func check(e error) {
 	}
 }
 
+type TwitterTrendsCache{
+	Woeid int `json: "woeid"`
+	TrendData string `json: trendata`
+}
+
 func main() {
 	anaconda.SetConsumerKey(_CONSUMER_KEY)
 	anaconda.SetConsumerSecret(_CONSUMER_SECRET)
 	api := anaconda.NewTwitterApi(_APP_TOKEN, _APP_TOKEN_SECRET)
-	searchResult, _ := api.GetTrendsByPlace(1, nil)
-	for _, tweet := range searchResult.Trends {
-		fmt.Println(tweet.Name)
+	for {
+		searchResult, _ := api.GetTrendsByPlace(1, nil)
+		fmt.Println(searchResult.Trends)
+		for _, tweet := range searchResult.Trends {
+			fmt.Println(tweet.Name)
+		}
+		time.Sleep(time.Minute * 5)
 	}
 }
