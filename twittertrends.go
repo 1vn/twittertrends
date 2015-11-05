967cafd5a8f57851759f0da35d79114dca734189
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-11-05 12:57:01 -0500

    cache twitter json

--- twittertrends.go
+++ twittertrends.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/chimeracoder/anaconda"
-	"time"
-	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 func check(e error) {
@@ -14,8 +14,8 @@ func check(e error) {
 	}
 }
 
-type TwitterTrendsCache{
-	Woeid int `json: "woeid"`
+type TwitterTrendsCache struct {
+	Woeid     int    `json: "woeid"`
 	TrendData string `json: trendata`
 }
 
@@ -23,12 +23,20 @@ func main() {
 	anaconda.SetConsumerKey(_CONSUMER_KEY)
 	anaconda.SetConsumerSecret(_CONSUMER_SECRET)
 	api := anaconda.NewTwitterApi(_APP_TOKEN, _APP_TOKEN_SECRET)
+	db, err := sql.Open("mysql", _DB_USER+":"+_DB_PASS+"@/"+_DB_NAME)
+	if err {
+		panic(err)
+	}
+	defer db.Close()
+
 	for {
 		searchResult, _ := api.GetTrendsByPlace(1, nil)
 		fmt.Println(searchResult.Trends)
 		for _, tweet := range searchResult.Trends {
 			fmt.Println(tweet.Name)
 		}
+		stmtIns, err := db.Prepare("INSERT INTO twittertrends VALUES(" + searchResult + ")")
+		defer stmtIns.close()
 		time.Sleep(time.Minute * 5)
 	}
 }
